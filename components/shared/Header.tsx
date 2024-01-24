577831d8e22389cedd34afd67de23d9167c2caa9
Kyryloloshka <kirill.contrmptuous@gmail.com>
2024-01-24 21:39:14 +0200

    create nav items, create sheet on mobile devices.

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -3,23 +3,29 @@ import Link from "next/link"
 import { Button } from "../ui/button"
 import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
 import NavItems from "./NavItems"
+import MobileNav from "./MobileNav"
 
 const Header = () => {
   return (
     <header className="w-full border-b">
       <div className="wrapper flex items-center justify-between">
         <Link href={"/"} className="w-36">
-          <Image src="/assets/logo/logo.svg" width={150} height={35} alt="Mimi logo" />
+          <Image 
+            src="/assets/logo/logo.svg"
+            width={100}
+            height={38} 
+            alt="Mimi logo" 
+          />
         </Link>
         <SignedIn>
-          <nav className="md:flex-between hidden w-full max-w-xs">
+          <nav className="md:flex-between hidden w-full">
             <NavItems />
           </nav>
         </SignedIn>
         <div className="flex w-32 justify-end gap-3">
           <SignedIn>
             <UserButton afterSignOutUrl="/"/>
-            <NavItems/>
+            <MobileNav />
           </SignedIn>
           <SignedOut>
             <Button asChild className="rounded-full" size="lg">
